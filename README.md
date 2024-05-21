73c1616c7881bb33e657da806fed02134a1211dc
MikkoVasankari <vasankarimikko@gmail.com>
2024-05-21 11:05:08 +0300

    Started creating installation/build script for project

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # mankka
 Go CLI application to play media files with MPV.io
 
-Requires that [mpv.io](https://mpv.io/) is installed on the system
+Requirements: <br>
+[mpv.io](https://mpv.io/) <br>
+[Go](https://go.dev/dl/)
 
-
+### To build/run the project
 To run use:
 ``` 
 go run main.go
 ```
-or build/run the executable
+or build the executable
 
 ```
 go build
 ``` 
 
-Tested to work with both Linux and Windows
 
 ### TODO
 - [ ] Create a check for file comptability with by mpv.io
-- [ ] Create installation script
---
+++ install/install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+install_mpv() {
+    if command_exists apt-get; then
+        echo "Installing mpv using apt-get..."
+        sudo apt-get update
+        sudo apt-get install -y mpv
+    elif command_exists dnf; then
+        echo "Installing mpv using dnf..."
+        sudo dnf install -y mpv
+    else
+        echo "Unsupported package manager. Please install mpv manually."
+        exit 1
+    fi
+}
+
+if command_exists mpv; then
+    echo "mpv is installed."
+else
+    echo "mpv is not installed. Installing mpv..."
+    install_mpv
+    if ! command_exists mpv; then
+        echo "Failed to install mpv. Please install it manually."
+        exit 1
+    fi
+fi
+
+PROJECT_DIR=$(pwd) 
+OUTPUT_BINARY="mankka"
+
+cd "$PROJECT_DIR" || { echo "Failed to change directory"; exit 1; }
+
+echo "Building Go project..."
+go build -o "$OUTPUT_BINARY"
+
+if [ $? -eq 0 ]; then
+    echo "Build succeeded. Binary created: $OUTPUT_BINARY"
+else
+    echo "Build failed."
+    exit 1
+fi
